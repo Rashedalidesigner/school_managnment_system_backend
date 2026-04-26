27dc75071c8ee068bca8eb9f66e283e284300676
rashed ali <rashedalidesigner@gmail.com>
2026-04-26 18:23:13 +0600

    fix

--- Index.js
+++ Index.js
@@ -17,7 +17,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 8000;
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -36,7 +36,6 @@ app.use("/api/fee", FeeRouter);
 
 connectDB().then(async () => {
     connectDB();
-    // console.log("Connected to MongoDB");
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
     });
